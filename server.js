60e4b6fa77a5028370c41d959b4cfad6993f83b0
roylines <roy@roylines.co.uk>
2012-08-20 22:55:48 +0100

    altered to use express 3 syntax

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var port = process.env.PORT || 5000;
+var app = express();
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello');
 });
 
-var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
