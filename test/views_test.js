f2d8ffa435e2ac52ebfaeb07985cfa88e40e0b9d
roylines <roy@roylines.co.uk>
2012-09-12 20:12:52 +0100

    added tests for rendering the signup page

--- test/views_test.js
+++ test/views_test.js
@@ -1,4 +1,5 @@
 var assert = require('assert'),
+    recaptcha = require('../lib/recaptcha.js'),
     sinon = require('sinon'),
     views = require('../lib/views.js');
 
@@ -8,6 +9,7 @@ describe('views', function() {
 
   beforeEach(function() {
     response.render = sinon.stub();
+    request.body = 'THEBODY';
   });
 
   describe('home', function() {
@@ -15,16 +17,47 @@ describe('views', function() {
       views.home(request, response);
     });
     it('should call render once', function() {
-      views.home(null, response);
+      views.home(request, response);
       assert.equal(response.render.calledOnce, true);
     });
     it('should pass render the correct view', function() {
-      views.home(null, response);
+      views.home(request, response);
       assert.equal(response.render.args[0][0], 'home');
     });
     it('should pass render the correct meta', function() {
-      views.home(null, response);
-      assert.deepEqual(response.render.args[0][1], {"title":"BendyBoards"});
+      views.home(request, response);
+      assert.deepEqual(response.render.args[0][1], {title : "BendyBoards"});
+    });
+  });
+
+  describe('signup', function() {
+    before(function() {
+      sinon.stub(recaptcha, 'publicKey');
+      recaptcha.publicKey.returns('PUBKEY');
+      response.errors = 'ERRORS';
+    });
+    after(function() {
+      recaptcha.publicKey.restore();
+      response.errors = undefined;
+    });
+    it('should not error', function() {
+      views.signup(request, response);
+    });
+    it('should call render once', function() {
+      views.signup(request, response);
+      assert.equal(response.render.calledOnce, true);
+    });
+    it('should pass render the correct view', function() {
+      views.signup(request, response);
+      assert.equal(response.render.args[0][0], 'signup');
+    });
+    it('should pass render the correct meta', function() {
+      views.signup(request, response);
+      assert.deepEqual(response.render.args[0][1], {
+        title:"Sign up BendyBoards",
+        recaptcha:"PUBKEY",
+        errors:"ERRORS",
+        values:"THEBODY"});
     });
   });
 });
